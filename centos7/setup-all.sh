704aee32b925adeb450e2062768a60283f3fa403
Joaquin Menchaca <joaquin.menchaca@appdynamics.com>
2015-11-17 09:39:04 -0800

    update setup hosts/ssh_config script

--- centos7/setup-all.sh
+++ centos7/setup-all.sh
@@ -3,9 +3,12 @@
 SYSTEMS="chefserver work node"
 IPADDRESS=6
 
+touch /etc/ssh/ssh_config
+
 for SYSTEM in ${SYSTEMS}; do
-  ### CREATE GLOBAL SSH CONFIG
-  cat <<-CONFIG_EOF >> /etc/ssh/ssh_config
+  if ! grep -q -F "Host ${SYSTEM}" /etc/ssh/ssh_config; then
+    ### CREATE GLOBAL SSH CONFIG
+    cat <<-CONFIG_EOF >> /etc/ssh/ssh_config
 Host ${SYSTEM}
   StrictHostKeyChecking no
   UserKnownHostsFile /dev/null
@@ -14,8 +17,9 @@ Host ${SYSTEM}
   IdentityFile /vagrant/.vagrant/machines/${SYSTEM}/virtualbox/private_key
   PasswordAuthentication no
 CONFIG_EOF
+  fi
 
   ### CREATE HOSTS
-  grep -q -F ${SYSTEM} /etc/hosts || echo "192.168.50.${IPADDRESS} ${SYSTEM}" >> /etc/hosts
+  grep -q -F "192.168.50.${IPADDRESS} ${SYSTEM}" /etc/hosts || echo "192.168.50.${IPADDRESS} ${SYSTEM}" >> /etc/hosts
   ((IPADDRESS++))
 done
