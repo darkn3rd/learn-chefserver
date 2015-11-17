df9fe8fa1e848bcee5514da1319e8b6bae6d733e
jmenchaca <sauvecali@yahoo.com>
2015-11-17 15:57:39 -0800

    フェッチのスクリプトを更新しました

--- centos7/setup-chefdk.sh
+++ centos7/setup-chefdk.sh
@@ -2,6 +2,7 @@
 
 # Target Package
 PACKAGE=chefdk-0.10.0-1.el7.x86_64.rpm
+PKGPATH=/vagrant/packages/${PACKAGE}
 
 # Install Package If it Exists and not installed already
-[ -f /vagrant/${PACKAGE} ] && rpm -q chefdk > /dev/null || sudo rpm -Uvh /vagrant/${PACKAGE}
+[ -f ${PKGPATH} ] && rpm -q chefdk > /dev/null 2>&1  || sudo rpm -Uvh ${PKGPATH}
--- centos7/setup-chefserver.sh
+++ centos7/setup-chefserver.sh
@@ -2,15 +2,17 @@
 
 # Target Package
 PACKAGE=chef-server-core-12.3.0-1.el7.x86_64.rpm
+PKGPATH=/vagrant/packages/${PACKAGE}
 
 # Install Package if it is available and not installed already
-[ -f /vagrant/${PACKAGE} ] && rpm -q chef-server-core > /dev/null || sudo rpm -Uvh /vagrant/${PACKAGE}
+[ -f ${PKGPATH} ] && rpm -q chef-server-core > /dev/null 2>&1 || sudo rpm -Uvh ${PKGPATH}
 
 # Set SELinux to Permissive
 [ "$(getenforce)" = "Permissive" ] || setenforce Permissive
 
 # Disable Qpid
 if $(rpm -qa | grep -q qpid); then
+  echo "Chef Server Setup: Disabling QPid"
   service qpidd stop
   chkconfig --del qpidd
 fi
