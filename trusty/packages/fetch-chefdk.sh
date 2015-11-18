e6506d04a705acd1925d92419732724356d47120
jmenchaca <sauvecali@yahoo.com>
2015-11-17 16:33:33 -0800

    フェッチのスクリプトが更新されました - ChefDK 0.10, ChefServer 12.3

--- trusty/packages/fetch-chefdk.sh
+++ trusty/packages/fetch-chefdk.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # NOTE: THIS IS RUN ON HOST SYSTEM
 BUCKET=opscode-omnibus-packages.s3.amazonaws.com
-PACKAGE=chefdk_0.9.0-1_amd64.deb
+PACKAGE=chefdk_0.10.0-1_amd64.deb
 PACKAGE_URL=https://${BUCKET}/ubuntu/12.04/x86_64/${PACKAGE}
 
 # Download Locally
--- trusty/packages/fetch-chefserver.sh
+++ trusty/packages/fetch-chefserver.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # NOTE: THIS IS RUN ON HOST SYSTEM
 #  This package works for Ubuntu 10.04, 12.04, 14.04
-PACKAGE=chef-server-core_12.2.0-1_amd64.deb
+PACKAGE=chef-server-core_12.3.0-1_amd64.deb
 URL=https://packagecloud.io/chef/stable/packages/ubuntu/trusty/${PACKAGE}/download
 
 # Test for Wget tool
